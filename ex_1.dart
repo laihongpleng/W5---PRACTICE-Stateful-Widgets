5b9ccf7250ef991665f54f0cd2c99f7b487059ee
laihongpleng <laihongpleng@gmail.com>
2025-11-21 03:06:26 +0700

    ex1
    Updated the main widget to include multiple selectable buttons arranged in a column. Added a SelectableButton class to manage button selection state.

--- ex_1.dart
+++ ex_1.dart
@@ -6,15 +6,54 @@ void main() => runApp(MaterialApp(
           title: const Text("Custom buttons"),
         ),
         body: Center(
-          child: SizedBox(
-                width: 400,
-                height: 100,
-                child: ElevatedButton(
-                    onPressed: () => {},
-                    child: const Center(
-                      child: Text("Not Selected"),
-                    )),
-              ),
-        ),
-      ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              SelectableButton(),
+              SizedBox(height: 20),
+              SelectableButton(),
+              SizedBox(height: 20),
+              SelectableButton(),
+            ],
+          ),
+      ),),
     ));
+class SelectableButton extends StatefulWidget {
+  const SelectableButton({super.key});
+
+  @override
+  State<SelectableButton> createState() => _SelectableButtonState();
+}
+
+class _SelectableButtonState extends State<SelectableButton> {
+  bool isSelected = false;
+  String get buttonText => isSelected ? "Selected" : "Not Selected";
+  Color get textColor => isSelected ? Colors.white : Colors.black;
+  Color get backgroundColor =>
+    isSelected ? Colors.blue[500]! : Colors.blue[50]!;
+
+
+
+  @override
+Widget build(BuildContext context) {
+  return SizedBox(
+    width: 400,
+    height: 100,
+    child: ElevatedButton(
+      onPressed: () {
+        setState(() {
+          isSelected = !isSelected; // reverse the value
+        });
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+      ),
+      child: Text(
+        buttonText,
+        style: TextStyle(color: textColor, fontSize: 20),
+      ),
+    ),
+  );
+}
+
+}
